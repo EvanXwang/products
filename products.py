7a272b657cc7fba07e1fcfaf1799892e743fdaab
Evan Wang <wangxevan@gmail.com>
2020-06-13 15:21:48 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,10 @@
+#while 迴圈適合用在不知會用幾次情況下
+products = []
+while True :
+	name = input ('請輸入商品名稱：')
+	if name == 'q':
+		break
+	price = input ('請輸入價格')
+	products.append([name,price]) #二維清單 （同個位置有多個input)
+print (products)
+
